6fed58859b2ef13fdcc5ab4af15837ccd86fe48d
Jared Goguen <jared.goguen@gmail.com>
2017-03-09 15:15:00 -0500

    added index page and remapped api root

--- connect/settings.py
+++ connect/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'games.apps.GamesConfig',
     'instances.apps.InstancesConfig',
     'players.apps.PlayersConfig',
+    'app.apps.AppConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -60,7 +61,7 @@ ROOT_URLCONF = 'connect.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['app/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- connect/urls.py
+++ connect/urls.py
@@ -1,13 +1,20 @@
-from django.conf.urls import url, include
-from rest_framework import routers
+import re
 
+from django.conf.urls import url, include
+
+from .routers import RedirectAPIRootRouter
 from .views import UserViewSet, GroupViewSet
 from games.views import GameViewSet
 from instances.views import InstanceViewSet
 from players.views import PlayerViewSet
 
+from app.views import IndexView
 
-router = routers.DefaultRouter()
+
+# Custom router so that '^$' can still be used as the home page.
+router = RedirectAPIRootRouter(r'^api/$')
+
+# Register all the api endpoints.
 router.register(r'api/users', UserViewSet)
 router.register(r'api/groups', GroupViewSet)
 router.register(r'api/games', GameViewSet)
@@ -15,8 +22,9 @@ router.register(r'api/instances', InstanceViewSet)
 router.register(r'api/players', PlayerViewSet)
 
 # Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
+# Include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
+    url(r'^$', IndexView.as_view(), name='index'),
     url(r'^api-auth/', include('rest_framework.urls'))
 ]
