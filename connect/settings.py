0353a7bb1072ea6baf2db615f600d11d4f6a592e
jared goguen <jared.goguen@gmail.com>
2017-08-23 21:03:04 -0400

    changing settings

--- connect/settings.py
+++ connect/settings.py
@@ -122,19 +122,20 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+DJANGO_ROOT = dirname(abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = join(DJANGO_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-    os.path.join(PROJECT_ROOT, 'app/build'),
-    os.path.join(PROJECT_ROOT, 'app/css'),
+    join(DJANGO_ROOT, 'static'),
+    join(DJANGO_ROOT, 'app/build'),
+    join(DJANGO_ROOT, 'app/css'),
 )
