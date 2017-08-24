3d3de6e348bda2b44c3a10f458ada638cbaf3739
jared goguen <jared.goguen@gmail.com>
2017-08-23 20:30:23 -0400

    changed settings

--- connect/settings.py
+++ connect/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = 'static/'
 STATICFILES_DIRS = [
     join(SITE_ROOT, 'app'),
 ]
