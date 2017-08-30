4939feb9dea471690d73d7792086d06c1ff19208
Jared Goguen <jared.goguen@gmail.com>
2017-08-30 11:28:54 -0400

    got registration to store hashed password

--- api/serializers.py
+++ api/serializers.py
@@ -2,7 +2,20 @@ from rest_framework import serializers
 
 from django.contrib.auth.models import User
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'email')
+        fields = ('username', 'email', 'password')
+
+    password = serializers.CharField(write_only=True)
+
+    def create(self, validated_data):
+
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
